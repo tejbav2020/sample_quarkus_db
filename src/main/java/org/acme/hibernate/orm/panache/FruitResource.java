b3070e95c40c51a4a64ad3b274320f79d8c3080c
tejbav2020 <76808229+tejbav2020@users.noreply.github.com>
2022-02-04 11:46:55 +0530

    Initial files added

---
+++ src/main/java/org/acme/hibernate/orm/panache/FruitResource.java
@@ -0,0 +1,40 @@
+package org.acme.hibernate.orm.panache;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import io.quarkus.hibernate.reactive.panache.Panache;
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Uni;
+
+@Path("/fruits")
+@ApplicationScoped
+public class FruitResource {
+
+	
+	@GET
+	public Uni<List<Fruit>> get() {
+	    return Fruit.listAll(Sort.by("name"));
+	}
+	
+	@GET
+	@Path("/{id}")
+	public Uni<Fruit> getSingle(Long id) {
+	    return Fruit.findById(id);
+	}
+	
+	@POST
+	public Uni<Response> create(Fruit fruit) {
+	    return Panache.<Fruit>withTransaction(fruit::persist)
+	            .onItem().transform(inserted -> Response.created(URI.create("/fruits/" + inserted.id)).build());
+	}
+	
+	
+	
+}
